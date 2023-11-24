5b034243b65a682661da158ead5662ca8c67102a
d0hyeon <ehgus6887@gmail.com>
2023-11-25 00:47:57 +0900

    fix: payload가 없는 상황에도 인자를 요구하는 타입 버그 수정

--- src/worker.ts
+++ src/worker.ts
@@ -1,16 +1,23 @@
 import { UtilWorker } from "./modules/UtilWorker";
 import { WorkerBuilder } from "./modules/WorkerBuilder";
 
-export function worker<Payload, ReturnValue>(fn: (payload: Payload) => ReturnValue) {
-  function createWorker() {
+type ReturnFn<ReturnValue> = () => ReturnValue;
+type ReturnFnWithPayload<Payload, ReturnValue> = (payload: Payload) => ReturnValue;
+
+export function worker<Payload = never, ReturnValue = void>(fn: (payload: Payload) => ReturnValue) {
+  function createWorker(): UtilWorker<Payload, ReturnValue> {
     return WorkerBuilder.fromModule(
-      UtilWorker,
+      UtilWorker<Payload, ReturnValue>,
       { module: fn }
     )
   }
 
   if (typeof window === 'undefined') {
-    return (payload: Payload) => createWorker().request(payload)
+    function runWithCreatingWorker(payload: any) {
+      return createWorker().request(payload);
+    }
+
+    return runWithCreatingWorker as Payload extends never ? ReturnFn<ReturnValue> : ReturnFnWithPayload<Payload, ReturnValue>;
   }
 
   return createWorker().request;
