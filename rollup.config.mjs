e4ceba6f7db34074ac287926ea53d9dd24b38d44
d0hyeon <ehgus6887@gmail.com>
2023-11-26 23:05:24 +0900

    chore: rollup 압축 플러그인 추가

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,7 @@
 
 import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+
 
 export default {
   input: 'src/index.ts',
@@ -8,6 +10,7 @@ export default {
     format: 'es'
   },
   plugins: [
-    typescript()
+    typescript(),
+    terser()
   ]
 };
