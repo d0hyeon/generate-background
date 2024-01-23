49ce66b4c098f4e3269286688c10f0566c3736b3
d0hyeon <ehgus6887@gmail.com>
2024-01-24 00:20:43 +0900

    delete: 미사용 메서드 제거

--- src/modules/UtilWorker.ts
+++ src/modules/UtilWorker.ts
@@ -1,20 +1,9 @@
 
-import { TypedMessageEvent, TypedWorker } from "./TypedWorker";
+import { ClonableType, TypedMessageEvent, TypedWorker } from "./TypedWorker";
 
-export class UtilWorker<Payload = never, Result = void> extends TypedWorker<Payload, Result> {
+export class UtilWorker<Payload extends ClonableType, Result extends ClonableType | void = void> extends TypedWorker<Payload, Result> {
   constructor(scriptURL: string | URL, options?: WorkerOptions) {
     super(scriptURL, options);
-  }
-
-  subscribe(observer: (response: Result) => void) {
-    const onMessage = (event: TypedMessageEvent<Result>) => {
-      observer(event.data);
-    };
-    this.addEventListener('message', onMessage);
-
-    return () => {
-      this.removeEventListener('message', onMessage);
-    };
   }
 
   request(payload: Payload) {
